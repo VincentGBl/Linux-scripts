1e8d41f8d4674897812067c25f418d3e20efd6c2
Gabriel Vicente de Lima <86087475+VincentGBl@users.noreply.github.com>
2026-04-02 10:46:04 -0300

    Update nfs_client.sh

--- nfs_client.sh
+++ nfs_client.sh
@@ -11,6 +11,7 @@ mkdir /mnt/Pasta_X
 mkdir /mnt/Pasta_Y
 
 #adiciona no fstab as linhas necessárias para se comunicar com os computadores-servidores com espaço compartilhado disponíveis nos laboratórios
-echo "X.X.X.X:/mnt/Storage_C1 /mnt/storage_C1 nfs defaults 0 0" >> /etc/fstab
-echo "X.X.X.X:/mnt/Storage_C4 /mnt/storage_C4 nfs defaults 0 0" >> /etc/fstab
+echo "X.X.X.X:/mnt/Storage_C1 /mnt/espaco_compartilhado1 nfs defaults 0 0" >> /etc/fstab
+echo "X.X.X.X:/mnt/Storage_C4 /mnt/espaco_compartilhado2 nfs defaults 0 0" >> /etc/fstab
 #substitua o "X.X.X.X" pelo endereço de IP da Máquina que está servindo como servidor NFS 
+#substitua o /mnt/espaco_compartilhadoX pelo caminho da pasta compartilhada no servidor NFS
