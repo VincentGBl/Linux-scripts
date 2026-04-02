4ea19dc80dbd1c7b2f538ef684ac06f6055a9138
Gabriel Vicente de Lima <86087475+VincentGBl@users.noreply.github.com>
2026-04-02 14:25:54 -0300

    Add files via upload

---
+++ nfs_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#instala os pacotes necessários para a utilização do servidor NFS
+sudo apt install nfs-kernel-server -y
+
+#cria a pasta que será compartilhada pela rede
+sudo mkdir -p /mnt/espaco_compartilhadoX
+
+#remove a configuração base de pertencimento a usuário/grupo da pasta
+sudo chown nobody:nogroup /mnt/espaco_compartilhadoX
+sudo chmod 777 /mnt/espaco_compartilhadoX
+
+#adiciona as regras de utilização e a pasta a ser compartilhada
+echo "/mnt/espaco_compartilhadoX Y.Y.Y.Y/Z(rw,sync,no_subtree_check)" >> /etc/exports
+#Y.Y.Y.Y(Endereço de rede)/Z(Máscara de rede)
+
+sudo exportfs -a
+sudo systemctl restart nfs-kernel-server
