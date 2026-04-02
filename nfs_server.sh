2882feb6068f4cbdedddb3eb5059078d64166041
Gabriel Vicente de Lima <86087475+VincentGBl@users.noreply.github.com>
2026-04-02 12:48:56 -0300

    Update nfs_server.sh

--- nfs_server.sh
+++ nfs_server.sh
@@ -8,3 +8,10 @@ sudo mkdir -p /mnt/espaco_compartilhadoX
 
 #remove a configuração base de pertencimento a usuário/grupo da pasta
 sudo chown nobody:nogroup /mnt/espaco_compartilhadoX
+
+#adiciona as regras de utilização e a pasta a ser compartilhada
+echo "/mnt/espaco_compartilhadoX Y.Y.Y.Y/Z(rw,sync,no_subtree_check)" >> /etc/exports
+#Y.Y.Y.Y(Endereço de rede)/Z(Máscara de rede)
+
+sudo exportfs -a
+sudo systemctl restart nfs-kernel-server
